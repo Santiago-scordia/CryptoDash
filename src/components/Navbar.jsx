a93a2a6c928d3a22b2b5c488e2d269fa421b863e
Santiago-scordia <santiscordia@est.frba.utn.edu.ar>
2022-05-24 03:13:05 -0300

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,25 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import logo from '../assets/logo.png';
 
 
 function NavBar() {
     return (
         <>
-            <Navbar bg="dark" variant="dark">
-                <Container>
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-                </Nav>
-                </Container>
-            </Navbar>
-            <br />
+        
             <Navbar bg="primary" variant="dark">
                 <Container>
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <img
+                     alt="Logo"
+                     src={logo}
+                     width="40"
+                     height="auto"
+                     className="d-inline-block"
+                     />
+                <Navbar.Brand href="#home">CryptoDash</Navbar.Brand>
                 <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-                </Nav>
-                </Container>
-            </Navbar>
-
-            <br />
-            <Navbar bg="light" variant="light">
-                <Container>
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
+                <Nav.Link href="#home">Graficos</Nav.Link>
+                <Nav.Link href="#features">Tablas</Nav.Link>
+                <Nav.Link href="#pricing">Tops</Nav.Link>
                 </Nav>
                 </Container>
             </Navbar>
