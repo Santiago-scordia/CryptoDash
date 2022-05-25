c342dc060be544dfb9a84871d5fdd1a441ca308f
Santiago-scordia <santiscordia@est.frba.utn.edu.ar>
2022-05-25 20:58:30 -0300

    Btc Tracker

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,23 +3,18 @@ import logo from '../assets/logo.png';
 
 
 function NavBar() {
+    
     return (
         <>
         
             <Navbar bg="primary" variant="dark">
                 <Container>
-                <img
-                     alt="Logo"
-                     src={logo}
-                     width="40"
-                     height="auto"
-                     className="d-inline-block"
-                     />
+                <img alt="Logo" src={logo} width="40" height="auto" className='logo' />
                 <Navbar.Brand href="#home">CryptoDash</Navbar.Brand>
-                <Nav className="me-auto">
-                <Nav.Link href="#home">Graficos</Nav.Link>
-                <Nav.Link href="#features">Tablas</Nav.Link>
-                <Nav.Link href="#pricing">Tops</Nav.Link>
+                <Nav className="me-auto ">
+                <Nav.Link href="#graphics" className='text-navbar'>Graficos</Nav.Link>
+                <Nav.Link href="#tables">Tablas</Nav.Link>
+                <Nav.Link href="#tops">Tops</Nav.Link>
                 </Nav>
                 </Container>
             </Navbar>
