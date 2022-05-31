344413f18d7783694eb9c9cdcd8b7667d461f684
Santiago-scordia <santiscordia@est.frba.utn.edu.ar>
2022-05-31 12:36:35 -0300

    Api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import React, {useState, useEffect} from 'react';
 import NavBar from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SerchCoins from './components/SerchCoins';
+
+
 
 function App() {
-  return(
-      <NavBar />
 
+  return (
+    <>
+    <NavBar/>
+    <SerchCoins />
+    </>
   );
 }
 
