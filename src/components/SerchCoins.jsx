b62dc507ad31c5e53b8fae34a5fa3f160a3c47ca
DidierLatreille <didier.latreille@gmail.com>
2022-06-08 23:49:43 -0300

    cambio tiempo consulta api

--- src/components/SerchCoins.jsx
+++ src/components/SerchCoins.jsx
@@ -17,7 +17,7 @@ function SerchCoins() {
           setCoins(res.data);
         })
         .catch(error => console.log(error));
-      }, 3400)
+      }, 2500)
 
       return () => clearInterval(intervalId);
     })
